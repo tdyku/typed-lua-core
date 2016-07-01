ac5eb6afea2030ed10c9de6244187b1fc6df6878
tomkee <tiomek93@gmail.com>
2016-07-01 12:23:31 +0200

    Subtyping tables. Preparing T-Table2

--- src/Typechecker/Subtype.hs
+++ src/Typechecker/Subtype.hs
@@ -2,7 +2,7 @@ module Typechecker.Subtype where
 
 import Data.Maybe (isNothing, fromJust)
 
-import Types             (F(..), L(..), B(..), P(..), S(..), T(..), E(..), V(..))
+import Types             (F(..), L(..), B(..), P(..), S(..), T(..), E(..), V(..), TType(..))
 import AST               (Expr(..), Stm(..), Block(..), LHVal(..), ExprList(..))
 import Typechecker.Utils (allT, anyT)
 
@@ -12,19 +12,90 @@ class Subtype a where
 
 
 instance Subtype F where
-    (FL LFalse     ) <? (FB BBoolean) = True
-    (FL LTrue      ) <? (FB BBoolean) = True
-    (FL (LString _)) <? (FB BString ) = True
-    (FL (LInt    _)) <? (FB BInt    ) = True
-    (FL (LInt    _)) <? (FB BNumber ) = True
-    (FL (LFloat  _)) <? (FB BNumber ) = True
-    (FB (BInt     )) <? (FB BNumber ) = True
-    _                <?  FValue       = True
-    _                <?  FAny         = True
-    FAny             <?  _            = True
-    FUnion fs        <? x             = allT $ fmap (<? x) fs
-    x                <? FUnion fs     = anyT $ fmap (x <?) fs
-    x                <? y             = x == y
+    (FL LFalse       )    <? (FB BBoolean   )    = True
+    (FL LTrue        )    <? (FB BBoolean   )    = True
+    (FL (LString _  ))    <? (FB BString    )    = True
+    (FL (LInt    _  ))    <? (FB BInt       )    = True
+    (FL (LInt    _  ))    <? (FB BNumber    )    = True
+    (FL (LFloat  _  ))    <? (FB BNumber    )    = True
+    (FB  BInt        )    <? (FB BNumber    )    = True
+    t1@(FTable ts1 tt1  ) <? t2@(FTable ts2 tt2) | tt1 == Unique && tt2 == Closed = sTable2 t1 t2
+                                                 | tt1 `elem` [Fixed, Closed] && tt2 == Closed = sTable1 t1 t2 
+                                                 | tt1 == Unique && tt2 `elem` [Unique, Open, Fixed] = sTable3 t1 t2
+                                                 | tt1 == Open && tt2 == Closed = sTable4 t1 t2
+                                                 | tt1 == Open && tt2 `elem` [Open, Fixed] = sTable5 t1 t2
+                                                 | tt1 == Fixed && tt2 == Fixed = sTable6 t1 t2
+    _                     <?  FValue             = True
+    _                     <?  FAny               = True
+    FAny                  <?  _                  = True
+    FUnion fs             <? x                   = allT $ fmap (<? x) fs
+    x                     <? FUnion fs           = anyT $ fmap (x <?) fs
+    x                     <? y                   = x == y
+
+
+sTable1, sTable2, sTable3, sTable4, sTable5, sTable6 :: F -> F -> Bool
+sTable1 (FTable lefts tt1) (FTable rights tt2) =
+    let rule (f', v') (f, v) = f <? f' && f' <? f && v `cSub` v'
+        firstLaw = fmap (\x -> (fmap (rule x)  lefts)) rights
+        forEachExists = allT $ fmap anyT firstLaw
+    in  forEachExists
+
+
+sTable2 (FTable ts1 tt1) (FTable ts2 tt2) = 
+    let lefts  = zip3 ([0..]) (fst <$> ts1) (snd <$> ts1)
+        rights = zip3 ([0..]) (fst <$> ts2) (snd <$> ts2)
+        lrProd = [(x,y) | x <- lefts, y <- rights]
+        firstLaw ((_, f, v),(_, f', v')) = if f <? f' then v `uSub` v' else True
+        secondLaw ((i, f, v),(j, f', v')) = if f <? f' then not (VF FNil `oSub` v') else True 
+        condSubtyping1 = fmap firstLaw lrProd
+        condSubtyping2 = fmap secondLaw lrProd
+    in allT condSubtyping1 && allT condSubtyping2
+ 
+
+sTable3 (FTable ts1 tt1) (FTable ts2 tt2) = 
+    let lefts  = zip3 ([0..]) (fst <$> ts1) (snd <$> ts1)
+        rights = zip3 ([0..]) (fst <$> ts2) (snd <$> ts2)
+        lrProd = [(x,y) | x <- lefts, y <- rights]
+        rule1 (i,f,v) (j,f',v') = f <? f' && v `uSub` v'
+        firstLaw = fmap (\x -> (fmap (rule1 x) rights)) lefts
+        forEachExists = allT $ fmap anyT firstLaw
+        rule2 ((i, f, v),(j, f', v')) = if f <? f' then not (VF FNil `oSub` v') else True 
+        forEachDoesNotExist = allT $ fmap rule2 lrProd
+    in  forEachExists && forEachDoesNotExist
+
+
+sTable4 (FTable ts1 tt1) (FTable ts2 tt2) = 
+    let lefts  = zip3 ([0..]) (fst <$> ts1) (snd <$> ts1)
+        rights = zip3 ([0..]) (fst <$> ts2) (snd <$> ts2)
+        lrProd = [(x,y) | x <- lefts, y <- rights]
+        firstLaw ((_, f, v),(_, f', v')) = if f <? f' then v `cSub` v' else True
+        secondLaw ((i, f, v),(j, f', v')) = if f <? f' then not (VF FNil `oSub` v') else True 
+        condSubtyping1 = fmap firstLaw lrProd
+        condSubtyping2 = fmap secondLaw lrProd
+    in allT condSubtyping1 && allT condSubtyping2
+
+
+sTable5 (FTable ts1 tt1) (FTable ts2 tt2) = 
+    let lefts  = zip3 ([0..]) (fst <$> ts1) (snd <$> ts1)
+        rights = zip3 ([0..]) (fst <$> ts2) (snd <$> ts2)
+        lrProd = [(x,y) | x <- lefts, y <- rights]
+        rule1 (i,f,v) (j,f',v') = f <? f' && v `cSub` v'
+        firstLaw = fmap (\x -> (fmap (rule1 x) rights)) lefts
+        forEachExists = allT $ fmap anyT firstLaw
+        rule2 ((i, f, v),(j, f', v')) = if f <? f' then not (VF FNil `oSub` v') else True 
+        forEachDoesNotExist = allT $ fmap rule2 lrProd
+    in  forEachExists && forEachDoesNotExist
+
+
+sTable6 (FTable ts1 tt1) (FTable ts2 tt2) = 
+    let lefts  = zip3 ([0..]) (fst <$> ts1) (snd <$> ts1)
+        rights = zip3 ([0..]) (fst <$> ts2) (snd <$> ts2)
+        rule12 (i,f,v) (j,f',v') = f <? f' && f' <? f && v `cSub` v'
+        firstLaw = fmap (\x -> (fmap (rule12 x) rights)) lefts
+        secondLaw = fmap (\x -> (fmap (rule12 x) lefts)) rights
+        forEachExistsij = allT $ fmap anyT firstLaw
+        forEachExistsji = allT $ fmap anyT secondLaw
+    in  forEachExistsij && forEachExistsji
 
 
 instance Subtype S where
