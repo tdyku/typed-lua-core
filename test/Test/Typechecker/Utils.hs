b938bf610d691c48d2a040f456621f433dda856b
tomkee <tiomek93@gmail.com>
2016-08-12 17:39:36 +0200

    Tests now with comments

--- test/Test/Typechecker/Utils.hs
+++ test/Test/Typechecker/Utils.hs
@@ -1,17 +1,20 @@
 {-# LANGUAGE LambdaCase #-}
 module Test.Typechecker.Utils where
 
-import Text.Trifecta.Parser (parseFromFile)
-import Text.Trifecta.Result (Result(..))
+import           Text.Trifecta.Parser  (parseString)
+import           Text.Trifecta.Result  (Result(..))
 
 import Parser.Code           (pManyStm)
 import Transform.Globals     (runGlobalTransform)
 import Typechecker.Utils     (runTypechecker)
 import Typechecker.Type      (tBlock)
+import Parser.Comments       (preCompilation)
 
 typeCheck :: String -> IO (Either String ())
-typeCheck path = parseFromFile pManyStm path >>= \case
-                        Nothing -> return . Left $ "Cannot parse"
-                        Just res -> do
-                            transformedRes <- runGlobalTransform res
-                            runTypechecker transformedRes tBlock
+typeCheck fpath = do
+        withoutComments <- preCompilation fpath
+        case parseString pManyStm mempty withoutComments of
+            Failure _ -> return . Left $ "Cannot parse"
+            Success res -> do
+                transformedRes <- runGlobalTransform res
+                runTypechecker transformedRes tBlock
